012be98edb1d2968fd96f44351cbe7df8741dfc4
Evelyn  Lawrence <evelawrence07@gmail.com>
2023-06-26 00:16:26 -0400

    sales process

---
+++ story_content/5zcDt5COmJL_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":288,"text":"Narrator: As the sun sets on Haru’s day,"},{"start":2784,"text":"he makes a surprising discovery…"},{"start":5216,"text":"Haru: Wait…what? Did Julio just do what I think he did?"},{"start":9152,"text":"If he makes this sale this would be the highest MRR that I have ever seen."},{"start":12768,"text":"As a manager, what can I do to support him during this deal process?"},{"start":16544,"text":"Narrator: This is it…your big chance.  Remember everything you learned."},{"start":21792,"text":"Haru: Gosh, mister narrator NO PRESSURE or anything right?"},{"start":27008,"text":"Narrator: Sorry.  This is what happens when your entire job is being dramatic."},{"start":32288,"text":"Haru: Okay…."},{"start":34528,"text":"okay….stay calm Haru.  I know what to do here…"}]}]};
+    window.globalLoadJsAsset('story_content/5zcDt5COmJL_transcripts.js', JSON.stringify(data));
+})();
